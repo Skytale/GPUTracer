2de34978cfe9f44879b934c2d51e0f44182cdfeb
Peter Hofmann <scm@uninformativ.de>
2010-03-04 18:38:30 +0100

    Added GPL header to shader sources.

--- shader_vertex.glsl
+++ shader_vertex.glsl
@@ -1,3 +1,23 @@
+/*
+	Copyright 2010 Peter Hofmann
+
+	This file is part of GPUTracer.
+
+	GPUTracer is free software: you can redistribute it and/or modify it
+	under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	GPUTracer is distributed in the hope that it will be useful, but
+	WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+	General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with GPUTracer. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 varying vec3 eye, pos_on_plane;
 varying vec3 light1;
 
